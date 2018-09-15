ecc0f2a94297a0697a937e9fef89b7e975b7f6b2
Oranjoose <oranjoose@gmail.com>
2018-09-15 05:17:43 -0400

    New: Spritesheet go module, updated Platformer
    Added: entire Spritesheet go module functional through looping, reverse, validation, but not yet ping pong
    
    Upgraded Platformer to work with tool script (there is a bug in Godot where built-in scripts won't work with tool script features. Added feature to snap size to sibling sprite. Fixed other issues.

---
+++ go_modules/Platformer.gd
@@ -0,0 +1,92 @@
+tool
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+export var Gravity = 20
+export var Speed = 200
+export var JumpStrength = 500
+
+export (Vector2) var CollisionBounds = Vector2(10, 10) setget _set_collision_bounds
+export (bool) var Set_Bounds_To_Sibling_Sprite = false setget _set_bounds_to_sibling_sprite
+
+var motion = Vector2(0, 0)
+
+func _set_collision_bounds(value):
+	CollisionBounds = value
+
+	#this function runs before the collisionshape exists for some reason
+#	if not get_shape_owners().empty():
+#		shape_owner_clear_shapes(get_shape_owners()[0])
+#		var rectshape = RectangleShape2D.new()
+#		rectshape.extents = value
+#
+#		shape_owner_add_shape(get_shape_owners()[0], rectshape)
+	
+	#scale works, but maybe extents is better
+	#but extents appears to have a bug, where changing extents in one instance, changes it in the editor for the first
+		#instance of the Platformer module
+	self.scale = value
+	
+#	if self.get_child_count() > 0: #to prevent pesky "CollisionShape2D not found" error upon each save, and crash when game starts 
+#		$CollisionShape2D.shape.extents = CollisionBounds
+	#update()
+	
+func _set_bounds_to_sibling_sprite(value):
+	#Set_Bounds_To_Sibling_Sprite = value
+	var siblingSprite
+	if value:
+#		var parent = 
+		if get_parent():
+			for child in get_parent().get_children():
+				if child is Sprite:
+					siblingSprite = child
+					break
+		
+		if siblingSprite:
+			_set_collision_bounds(siblingSprite.texture.get_size() * siblingSprite.scale)
+		elif get_parent(): #to prevent the print from happening each time the module is saved (since no parent)
+			print("no sibling sprites")
+		
+	
+#func _draw():
+#	var rect = Rect2(0, 0, 200, 300)
+#
+#	draw_rect(rect, Color(randf(), randf(), randf()))
+
+func _ready():
+	#necessary to have child move the parent without perpetually throwing off the child in turn
+	if not Engine.editor_hint:
+		#necessary to have child move the parent without perpetually throwing off the child in turn
+		set_as_toplevel(true)
+		
+		#start at location where parent node was placed within level editor, then
+			#proceed to have parent follow child (KinematicBody2D) for position
+		self.global_position = get_parent().global_position
+	
+	_set_collision_bounds(CollisionBounds)
+	pass
+
+func _physics_process(delta):
+	if not Engine.editor_hint:
+		#first set to the parent's position, in case the parent was teleported, etc.
+			#in the last frame
+		self.global_position = get_parent().global_position
+		
+		motion.y += Gravity
+	
+		if Input.is_action_pressed("ui_right"):
+			motion.x = Speed
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -Speed
+		else:
+			motion.x = 0
+			
+		if self.is_on_floor():
+			if Input.is_action_just_pressed("ui_up"):
+				motion.y = -JumpStrength
+				
+		motion = move_and_slide(motion, UP)
+		
+		get_parent().global_position = self.global_position
+	
+	
---
+++ go_modules/Spritesheet.gd
@@ -0,0 +1,92 @@
+tool
+extends Sprite
+
+export (Texture) var SpritesheetTexture setget _set_texture
+
+export var CurrentFrame = 0 setget _set_current_frame
+export var Framerate = 12.0 setget _set_frame_rate
+export var HorzFrames = 1 setget _set_horz_frames
+export var VertFrames = 1 setget _set_vert_frames
+
+export var FirstFrame = 0 setget _set_first_frame
+export var FinalFrame = 1 setget _set_final_frame
+
+export var Loop = true setget _set_looping
+export var Reverse = false setget _set_reverse
+
+export var IsPlaying = false setget _set_playing
+export var Autostart = true setget _set_autostart
+
+var currentTime = 0
+var frameInterval = 1
+
+func _set_texture(value):
+	SpritesheetTexture = value
+	self.texture = value
+
+func _set_current_frame(value):
+	if value >= FirstFrame and value <= FinalFrame:
+		CurrentFrame = value
+		self.frame = value
+	
+func _set_frame_rate(value):
+	Framerate = value
+	frameInterval = 1.0/value
+	
+func _set_horz_frames(value):
+	if value > 0:
+		HorzFrames = value
+		self.hframes = value
+	
+func _set_vert_frames(value):
+	if value > 0:
+		VertFrames = value
+		self.vframes = value
+	
+func _set_first_frame(value):
+	if value >= 0 and value <= FinalFrame:
+		FirstFrame = value
+
+func _set_final_frame(value):
+	if value >= FirstFrame and value <= HorzFrames * VertFrames:
+		FinalFrame = value
+	else:
+		FinalFrame = HorzFrames * VertFrames
+	
+func _set_looping(value):
+	Loop = value
+	
+func _set_reverse(value):
+	Reverse = value
+	
+func _set_playing(value):
+	IsPlaying = value
+	
+func _set_autostart(value):
+	Autostart = value
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Autostart:
+		IsPlaying = true
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if IsPlaying:
+		currentTime += delta
+		
+		print(CurrentFrame)
+		if currentTime > frameInterval * (CurrentFrame + 1 if not Reverse else FinalFrame - (CurrentFrame - FirstFrame)):
+			if CurrentFrame == (FinalFrame if not Reverse else FirstFrame):
+				_set_current_frame(FirstFrame if not Reverse else FinalFrame)
+				currentTime = FirstFrame * frameInterval #reset clock
+				
+				if not Loop:
+					_set_playing(false)
+			else:
+				_set_current_frame(CurrentFrame + 1 if not Reverse else CurrentFrame - 1)
+			
+	
+	
+	pass
